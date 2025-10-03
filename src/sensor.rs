c1e1c35ecc954713b87b1ba9fa0d5e0305767236
Juan Esteban Guevara <juguevara@unal.edu.co>
2025-10-02 21:44:21 -0500

    fix(sensor.rs): fixed sensor code manually

--- src/sensor.rs
+++ src/sensor.rs
@@ -1,12 +1,18 @@
-use anyhow::{Context, Result};
 use std::fs::File;
-use std::io::Read;
+use std::io::{self, Read};
 
-pub fn read_sensor(device_path: &str) -> Result<u32> {
-    let mut file = File::open(device_path)
-        .context("Failed to open device")?;
+pub fn read_sensor(device_path: &str) -> Result<u32, io::Error> {
+    let mut file = match File::open(device_path) {
+        Ok(file) => file,
+        Err(err) => return Err(err),
+    };
     let mut buf = [0u8; 4];
-    file.read_exact(&mut buf)
-        .context("Failed to read 4 bytes from device")?;
+
+
+    match file.read_exact(&mut buf) {
+        Ok(_) => {},
+        Err(err) => return Err(err),
+    }
+
     Ok(u32::from_le_bytes(buf))
 }
